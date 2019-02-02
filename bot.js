36f7c2840c9ccc9b4144a6c746d7778b757ff633
GreeGhost123 <46789490+GreeGhost123@users.noreply.github.com>
2019-02-02 23:01:05 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,13 +1,29 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
-
-
-client.on("ready", () => {
-let channel =     client.channels.get("535494010656129044")
-setInterval(function() {
-channel.send(`sorry kandire spam bache itzad lia credits berojola matmss7eche had channel`);
-}, 30)
-})
-
-client.login(process.env.BOT_TOKEN);
+const Eris = require("eris");
+var x5bzteam = new Eris("NDYyMDI4ODQ0NTQzNTA4NDkw.DzZT3A.iStia_inktUg5zGXV01p9qnq1mA");
+x5bzteam.on("ready", () => {
+    console.log("Yo! Changer [ON]");
+    onstart();
+});
+x5bz = "484341267422117890";
+nick = "NiCho_Alonzo";
+function onstart(){
+    var v1 = nick.split('');
+    var counter;
+    var i=0;
+var x5bz2 = ' ';
+  var x5bz3 =   setInterval(function(){
+     
+     if (i == v1.length){
+     clearInterval(x5bz3);
+     onstart()
+      return;
+     }
+     var v2 = v1[i];
+    x5bz2 += v2;
+    x5bzteam.editNickname(x5bz, x5bz2);
+ 
+        i++;
+   
+     }, 1000);
+}
+x5bzteam.connect();
